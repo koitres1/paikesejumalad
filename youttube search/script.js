acaa7a6ab966ebf2d098fca734325c316ac1f594
Michael Kevin Karlson <57061414+ukawaii@users.noreply.github.com>
2021-11-15 16:49:25 +0200

    Add files via upload

---
+++ youttube search/script.js
@@ -0,0 +1,32 @@
+// JavaScript source code
+$(document).ready(function () {
+    var API_KEY = "AIzaSyCyCbw7U9k7LhTpyWCyGJW66xbbuvR1E1g"
+
+    var video = ''
+
+    $("form").submit(function (event) {
+        event.preventDefault()
+
+        var search = $("#search").val()
+
+        videoSearch(API_KEY,search,10)
+    })
+
+    function videoSearch(key, search, maxResults) {
+
+        $("#vidoes").empty()
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
+            console.log(data)
+
+            data.items.forEach(item => {
+                video = `
+
+                        <iframe width = "420" height = "315" src ="http://www.youtube.com/embed/${item.id.videoId}?controls=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+
+                        `
+                $("#vidoes").append(video)
+            }); //remove autoplay and control
+        })
+
+    }
+})
